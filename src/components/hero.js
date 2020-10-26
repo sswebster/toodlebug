db19cbb51a1f49eff724c4091b142831c2b1a699
Sam Webster <sam@secondhandlegends.com>
2020-10-26 10:23:29 -0400

    Menu and header updates

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,15 +5,10 @@ import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={data.name}
-      fluid={data.heroImage.fluid}
-    />
+
     <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
+      <h1 className={styles.heroHeadline}>Taking Care of a ToodleBug</h1>
+      <p></p>
     </div>
   </div>
 )
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,12 +5,15 @@ import styles from './navigation.module.css'
 export default () => (
   <nav role="navigation">
     <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
+		<li className={styles.navigationItem}>
+			<Link to="/">Home</Link>
+		</li>
+		<li className={styles.navigationItem}>
+			<Link to="/sammy/">Sammy</Link>
+		</li>
+		<li className={styles.navigationItem}>
+			<Link to="/boyden/">Boyden</Link>
+		</li>
     </ul>
   </nav>
 )
